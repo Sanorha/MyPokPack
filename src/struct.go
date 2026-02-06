84e6fef45c56ba7b47729819c0715ecb9a299d07
daemma <emma.darmon@ynov.com>
2026-02-06 14:28:51 +0100

    strcut

--- src/struct.go
+++ src/struct.go
@@ -6,6 +6,14 @@ type User struct {
 	Email      string
 }
 
+type Jeux struct {
+	Name  string `json:"name"`
+	Types string `json:"types"`
+}
+
 func InitUser() User {
 	return User{}
+}
+func InitJeux() Jeux {
+	return Jeux{}
 }
